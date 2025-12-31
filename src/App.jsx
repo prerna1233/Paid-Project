61bb42f7fa06a780dab8834ee778ad6814cc5e9d
Nikita Kumari <nikita24@navgurukul.org>
2025-12-31 11:22:01 +0530

    Signup feature added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css";
 import Navbar from './Components/Navbar/Navbar';
 import Approutes from './Routes/Approutes';
+import Signup from './Components/Signup/Signup';
 
 export default function App() {
   return (
    <>
    <Navbar />
    <Approutes />
+   <Signup />
    </>
   )
 }
