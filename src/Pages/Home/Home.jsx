5924e2c64da794e14638b3748af9b82b072bf3f2
bhawna22@navgurukul.org <bhawna22@navgurukul.org>
2026-01-03 03:12:56 +0530

    banner positioned

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,41 +9,42 @@ export default function Home() {
   const title = "Explore Kishanganj"
 
   return (
-    <div className="hero-section">
-      <video src={homeVideo} autoPlay muted loop playsInline className="hero-video" />
+    <>
+      <div className="hero-section">
+        <video src={homeVideo} autoPlay muted loop playsInline className="hero-video" />
 
-      {/* LEFT SIDE TEXT */}
-      <div className="hero-text left">
-        <h1>
-          {title.split("").map((char, index) => (
-            <span
-              key={index}
-              className="fall-letter"
-              style={{ animationDelay: `${index * 0.08}s` }}
-            >
-              {char === " " ? "\u00A0" : char}
-            </span>
-          ))}
-        </h1>
+        {/* LEFT SIDE TEXT */}
+        <div className="hero-text left">
+          <h1>
+            {title.split("").map((char, index) => (
+              <span
+                key={index}
+                className="fall-letter"
+                style={{ animationDelay: `${index * 0.08}s` }}
+              >
+                {char === " " ? "\u00A0" : char}
+              </span>
+            ))}
+          </h1>
 
-        <p className="subtitle">Nature • Culture • Peace</p>
-        <button className='btn'>Explore<FaArrowRight /></button>
-      </div>
-      <hr />
-      <div className='hero-section-para'>
-        <div className='para1'>
-          <h3>Kishanganj</h3>
-          <br />
-          <p>Kishanganj is a district and town located in the northeastern part of the Indian state of Bihar. It is known for its greenery, tea gardens, and its location near the borders of Nepal and West Bengal.</p>
+          <p className="subtitle">Nature • Culture • Peace</p>
+          <button className='btn'>Explore<FaArrowRight /></button>
         </div>
-        <div className='image-stack'>
-          <img className="image1" src='src/assets/1pic.png' alt="Kishanganj image 1" />
-          <img className="image2" src='src/assets/2pic.png' alt="Kishanganj image 2" />
-          <img className="image3" src='src/assets/3pic.png' alt="Kishanganj image 3" />
+        <hr />
+        <div className='hero-section-para'>
+          <div className='para1'>
+            <h3>Kishanganj</h3>
+            <br />
+            <p>Kishanganj is a district and town located in the northeastern part of the Indian state of Bihar. It is known for its greenery, tea gardens, and its location near the borders of Nepal and West Bengal.</p>
+          </div>
+          <div className='image-stack'>
+            <img className="image1" src='src/assets/1pic.png' alt="Kishanganj image 1" />
+            <img className="image2" src='src/assets/2pic.png' alt="Kishanganj image 2" />
+            <img className="image3" src='src/assets/3pic.png' alt="Kishanganj image 3" />
+          </div>
         </div>
       </div>
-      <Banner />
-      <Footer />
-    </div>
+      <Banner className="Banner" />
+    </>
   )
 }
