3e4723b418d5a8c5c7461b9a4f603ff57b84a6bf
Prerna <prernasai23@navgurukul.org>
2026-01-02 15:51:51 +0530

    Added GIF

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import Footer from '../../Components/Footer/Footer'
+import homeVideo from '../../assets/home1.mp4'
+import './Home.style.css'
 
 export default function Home() {
+  const title = "Explore Kishanganj"
+
   return (
-    <>
-     <div>This is Home</div>
-    </>
+    <div className="hero-section">
+      <video
+        src={homeVideo}
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="hero-video"
+      />
+
+      {/* LEFT SIDE TEXT */}
+      <div className="hero-text left">
+        <h1>
+          {title.split("").map((char, index) => (
+            <span
+              key={index}
+              className="fall-letter"
+              style={{ animationDelay: `${index * 0.08}s` }}
+            >
+              {char === " " ? "\u00A0" : char}
+            </span>
+          ))}
+        </h1>
+
+        <p className="subtitle">Nature • Culture • Peace</p>
+      </div>
+    </div>
   )
 }
