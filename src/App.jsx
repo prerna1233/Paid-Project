01d27014b678f7050b7717e02b51d77036816d94
bhawna22@navgurukul.org <bhawna22@navgurukul.org>
2026-01-07 19:33:57 +0530

    Revert "Merge branch 'Nikita' into bhawna-homepage for culture, explore(travel) and accomodation page"
    This reverts commit 82ee80385763797f06de1a7a1893f0222a5bc334, reversing
    changes made to 090ad379a913bedd3069b4bdf6f36ed0b830188d.

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import "./App.css";
 import Navbar from './Components/Navbar/Navbar';
 import Approutes from './Routes/Approutes';
-import Signup from './Components/Signup/Signup';
 
 export default function App() {
   return (
--- src/Pages/Accomodation/Accomodation.jsx
+++ src/Pages/Accomodation/Accomodation.jsx
@@ -1,16 +1,9 @@
-import React from "react";
-import "./Accomodation.style.css";
+import React from 'react'
 
 function Accomodation() {
   return (
-    <div className="coming-container">
-      <div className="coming-content">
-        <h1>Coming Soon</h1>
-       
-       
-      </div>
-    </div>
-  );
+    <div>Accomodation</div>
+  )
 }
 
-export default Accomodation;
+export default Accomodation
