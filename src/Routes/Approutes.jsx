e7c9a5cf7e4eb14903e008980289d864d779fa33
bhawna22@navgurukul.org <bhawna22@navgurukul.org>
2026-01-07 19:33:57 +0530

    Revert "Merge branch 'Nikita' into bhawna-homepage for culture, explore(travel) and accomodation page"
    This reverts commit 82ee80385763797f06de1a7a1893f0222a5bc334, reversing
    changes made to 090ad379a913bedd3069b4bdf6f36ed0b830188d.

--- src/Routes/Approutes.jsx
+++ src/Routes/Approutes.jsx
@@ -4,29 +4,20 @@ import About from '../Pages/About/About.jsx'
 import Accomodation from '../Pages/Accomodation/Accomodation.jsx'
 import Culture from '../Pages/Culture/Culture.jsx'
 import Blogs from '../Pages/Blogs/Blogs.jsx'
-import Signup  from '../Components/Signup/Signup.jsx'
 import Login from '../Components/Login/Login.jsx'
-import Travel from '../Pages/Travel/Travel.jsx'
 import { Routes, Route } from 'react-router-dom'
-
 
 export default function Approutes() {
   return (
     <>
-   
      <Routes>
-      
       <Route path='/login' element={<Login/>} />
         <Route path='/' element={<Home />} />
-        <Route path='/Signup' element={<Signup/>}/>
         <Route path='/About' element={<About />} />
         <Route path='/Accomodation' element={<Accomodation />} />
         <Route path='/Culture' element={<Culture />} />
         <Route path='/Blogs' element={<Blogs />} />
-        <Route path="/travel" element={<Travel />} />
-
      </Routes>
-   
     </>
   )
 }
