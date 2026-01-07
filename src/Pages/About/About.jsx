090ad379a913bedd3069b4bdf6f36ed0b830188d
bhawna22@navgurukul.org <bhawna22@navgurukul.org>
2026-01-07 19:23:21 +0530

    about page started

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,7 +1,72 @@
 import React from 'react'
+import './About.style.css'
 
 export default function About() {
   return (
-    <div>About</div>
+    <>
+      <div className='first'>
+        <img src="src/assets/bg-about.jpg" alt="Traditional Food" className='culture-img1' />
+      </div>
+      <hr className="bottom-hr" />
+
+      <div className="heading1">
+        <h1>Leadership &  Administeration</h1>
+      </div>
+    
+        <div className="aboutusphoto">
+      <div className="SEO">
+        <img src="src/assets/bdo.jpeg" alt="SEO Image" />
+        <h1>BDO</h1>
+        <p>Mr. Amarjit Kumar singh</p>
+        <p>
+          District Magistrate is officer of the district. The DM is responsible for maintaining law , implementing government policies.
+          <br />
+          <span>vishakrajDM@gmail.com</span>
+        </p>
+      </div>
+
+      <div className="CEO">
+        <img src="src/assets/CEO.jpg" alt="CEO Image" />
+        <h1>CEO</h1>
+        <p>Mr. Indradeo Paswan </p>
+        <p>
+          District Magistrate is officer of the district. The DM is responsible for maintaining law , implementing government policies.
+          <br />
+          <span>vishakrajDM@gmail.com</span>
+        </p>
+      </div>
+
+
+      <div className="IAS">
+        <img src="src/assets/DM.jpeg" alt="SEO Image" />
+        <h1>IAS</h1>
+        <p>Mr. Vishal Raj</p>
+        <p>
+          District Magistrate is officer of the district. The DM is responsible for maintaining law , implementing government policies.
+          <br />
+          <span>vishakrajDM@gmail.com</span>
+        </p>
+      </div>
+    </div>
+
+
+
+    <div className="disigners">
+      <div className="figma">
+        <img src="src/assets/team.jpeg" alt="team image" />
+        <h1>Ux/Figma designer Team </h1>
+        <p>Our Foigma designers Team focuses on clean , accessible, and user-friendly interfaces for digital platform <span> Moni Kumari , Sweta , Suman</span> collaboratively design structured  layouts, mantain visual 
+        consistency, and ensure clarity in user experience. Their combind efforts support effective communication, usability, and modern design standards across the project.
+          </p>
+      </div>
+    </div>
+
+
+
+
+
+    </>
+
+
   )
 }
