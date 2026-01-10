b25996b1a3b532bc8ab08ab8503ff1205393c933
Prerna <prernasai23@navgurukul.org>
2026-01-11 00:12:16 +0530

    worked on culture page

--- src/Routes/Approutes.jsx
+++ src/Routes/Approutes.jsx
@@ -6,20 +6,25 @@ import Culture from '../Pages/Culture/Culture.jsx'
 import Blogs from '../Pages/Blogs/Blogs.jsx'
 import Login from '../Components/Login/Login.jsx'
 import Travel from '../Pages/Travel/Travel.jsx'
+import Festivals from '../Pages/Culture/Festivals.jsx'
+import Art from '../Pages/Culture/Art.jsx'
+import Food from '../Pages/Culture/Food.jsx'
 import { Routes, Route } from 'react-router-dom'
 
 export default function Approutes() {
   return (
     <>
-     <Routes>
-      <Route path='/login' element={<Login/>} />
+      <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path='/' element={<Home />} />
         <Route path='/About' element={<About />} />
         <Route path='/Accomodation' element={<Accomodation />} />
-        <Route path='/Culture' element={<Culture />} />
+        <Route path='/Culture/festivals' element={<Festivals />} />
+        <Route path='/Culture/art' element={<Art />} />
+        <Route path='/Culture/food' element={<Food />} />
         <Route path='/Blogs' element={<Blogs />} />
         <Route path='/Travel' element={<Travel />} />
-     </Routes>
+      </Routes>
     </>
   )
 }
