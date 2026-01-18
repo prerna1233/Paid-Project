346c0ad7329bbba2b75d38437420a93505e2abf2
Sunitakumari24 <12skumari@gmail.com>
2026-01-18 22:31:58 +0530

    cards

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Cards.css';
+function Cards() {
+  return (
+    <div className='card-section'>
+      <div className='card'>
+      
+      </div>
+    </div>
+  )
+}
+export default Cards;
