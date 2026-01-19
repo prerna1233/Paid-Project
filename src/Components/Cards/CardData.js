2991c138e0adec224c9c528db85fe36ad69393e9
bhawna22@navgurukul.org <bhawna22@navgurukul.org>
2026-01-19 12:14:48 +0530

    cards modified

---
+++ src/Components/Cards/CardData.js
@@ -0,0 +1,54 @@
+const cardData = [
+  {
+    image: "/src/assets/aloo_bhujiya.png",
+    title: "Card 1",
+    description: "This is the description for Card 1.",
+  },
+  {
+    image: "https://via.placeholder.com/300",
+    title: "Card 2",
+    description: "This is the description for Card 2.",
+  },
+  {
+    image: "/src/assets/baigan_bhaja.png",
+    title: "Card 3",
+    description: "This is the description for Card 3.",
+  },
+  {
+    image: "/src/assets/aloo_bhujiya.png",
+    title: "Card 4",
+    description: "This is the description for Card 4.",
+  }
+//   {
+//     image: "/src/assets/aloo_bhujiya.png",
+//     title: "Card 1",
+//     description: "This is the description for Card 1.",
+//   },
+//   {
+//     image: "/src/assets/aloo_bhujiya.png",
+//     title: "Card 1",
+//     description: "This is the description for Card 1.",
+//   },
+//   {
+//     image: "/src/assets/aloo_bhujiya.png",
+//     title: "Card 1",
+//     description: "This is the description for Card 1.",
+//   },
+//   {
+//     image: "/src/assets/aloo_bhujiya.png",
+//     title: "Card 1",
+//     description: "This is the description for Card 1.",
+//   },
+//   {
+//     image: "/src/assets/aloo_bhujiya.png",
+//     title: "Card 1",
+//     description: "This is the description for Card 1.",
+//   },
+//   {
+//     image: "/src/assets/aloo_bhujiya.png",
+//     title: "Card 1",
+//     description: "This is the description for Card 1.",
+//   }
+];
+
+export default cardData;
