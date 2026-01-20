822c7647b1e4e734c5fc8a863b8eb3f9683989b2
bhawna22@navgurukul.org <bhawna22@navgurukul.org>
2026-01-20 11:24:02 +0530

    accomodation and photos for avatars and food page

--- src/Pages/Accomodation/Accomodation.jsx
+++ src/Pages/Accomodation/Accomodation.jsx
@@ -6,11 +6,25 @@ import Footer from "../../Components/Footer/Footer.jsx";
 function Accomodation() {
   return (
     <>
-    <div>
-      <div className="accomodation-page-Hero-Section">
-      </div>
-      <p className="texxxxd">Hotels</p>
-      <div>search bar</div>
+      <div>
+        <div className="accomodation-page-top-section">
+          <div className="accomodation-page-top-section-content">
+            <h2>Kishanganj hotels</h2>
+            <input className="location" id="location" placeholder="Enter your location"></input>
+            <div className="date" >
+              <input placeholder="Start date"></input>
+              <input placeholder="End date"></input>
+            </div>
+            <input className="guests" placeholder="No. of guests"></input>
+            box white
+            Kishanganj hotels heading1
+            location input
+            date to and fro calender input
+            guests input
+            search btn
+          </div>
+        </div>
+        {/* <p className="texxxxd">Hotels</p> */}
       </div>
       <div className="card-section">
         <Cards />
