3ec9500e3fb4917091e6b87e3b1f7d1625f5e1ec
aasiya <aasiya24@navgurukul.org>
2026-01-22 23:17:38 +0530

    Add the Accomodation page

---
+++ src/Pages/Accomodation/Accomodation.js
@@ -0,0 +1,43 @@
+
+const placesData = [
+  {
+    id: 1,
+    title: "Kishanganj",
+    img:"src/assets/kne.jpeg",
+    desc: "Kishanganj is a district in northeastern Bihar, known for its lush greenery, tea gardens, and diverse culture. Located near West Bengal and Nepal, it is one of the most scenic regions of Bihar with rich history, harmony, and agricultural importance."
+  },
+  {
+    id: 2,
+    title: "Thakurganj",
+    img: "src/assets/thakurganj.jpeg",
+    desc: "Thakurganj is a prominent town in the Kishanganj district of Bihar, located near the Nepal border. It is known for greenery, tea gardens, and a peaceful environment."
+  },
+  {
+    id: 3,
+
+    title: "Darjeeling",
+    img: "src/assets/Pasted image (2).png"
+  },
+  {
+    id: 4,
+    title: "West Bengal",
+    img: "src/assets/Pasted image (3).png",
+    desc: "West Bengal is an eastern Indian state known for rich culture, history, literature, festivals, and natural beauty."
+  },
+  {
+    id: 5,
+    title: "Kanki",
+    img: "src/assets/Pasted image (4).png",
+    desc: "Kanki is a small town in Kishanganj district of Bihar, known for peaceful surroundings, greenery, and Kanki River."
+  },
+  {
+    id: 6,
+    title: "Islampur",
+    img: "src/assets/Pasted image (5).png",
+    desc: "Islampur is a town in North Dinajpur district of West Bengal, near Bihar border, known for markets and mixed culture."
+  }
+];
+
+export default placesData;
+
+
--- src/Pages/Accomodation/Accomodation.jsx
+++ src/Pages/Accomodation/Accomodation.jsx
@@ -1,16 +1,38 @@
 import React from "react";
+import Footer from "../../Components/Footer/Footer";
+import placesData from "./Accomodation.js";
 import "./Accomodation.style.css";
+import Bihar from "../Boxmain2/Bihar";
+import { Link } from 'react-router-dom';
+
+
 
 function Accomodation() {
   return (
-    <div className="coming-container">
-      <div className="coming-content">
-        <h1>Coming Soon</h1>
-       
-       
+    <>
+      <div className="sestion">
+        <img src="src/assets/explorebg.jpg" alt="background" />
+        <hr />
       </div>
-    </div>
+
+      <div className="sestion2">
+        {placesData.map((place) => (
+          <div className="boxmain" key={place.id}>
+            <img src={place.img} alt={place.title} />
+            <h2>{place.title}</h2>
+            <p>{place.desc}</p>
+            <Link to="/Bihar" style={{textDecoration: "none"}}>
+            <button>Read More</button>
+            </Link>
+           
+          </div>
+        ))}
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
 export default Accomodation;
+
